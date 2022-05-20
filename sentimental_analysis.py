faf006e05bd1e5288f0731e56529bc9cc44ffa41
yasirn08 <50393848+yasirn08@users.noreply.github.com>
2022-05-20 14:30:37 +0600

    compiler

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -124,3 +124,6 @@ model.add(LSTM(200, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(128, activation="relu"))
 model.add(Dense(1,activation="sigmoid"))
 
+model.compile(optimizer = "adam", loss = "binary_crossentropy", metrics=["accuracy"])
+model.summary()
+
